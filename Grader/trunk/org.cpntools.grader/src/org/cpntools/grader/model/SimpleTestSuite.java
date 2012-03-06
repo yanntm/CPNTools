d536a57ad605dcfcb80a46910fee2586984089f8
mwesterg <michael.westergaard@cs.au.dk>
2012-03-06 16:17:40 +0000

    Significans loading ans saving improvements

--- Grader/trunk/org.cpntools.grader/src/org/cpntools/grader/model/SimpleTestSuite.java
+++ Grader/trunk/org.cpntools.grader/src/org/cpntools/grader/model/SimpleTestSuite.java
@@ -7,7 +7,7 @@ public class SimpleTestSuite extends TestSuite {
 		super(new SignatureGrader(-1.0, threshold, secret));
 		graders.add(new NameCategorizer(-0.1));
 		graders.add(new DeclarationSubset(-0.5, true, 0.0));
-		graders.add(new InterfacePreservation(-0.5, 0.0, true, false, Collections.singletonList("New Page"),
-		        Collections.<String> emptyList()));
+		graders.add(new InterfacePreservation(-0.5, -0.2, true, false, Collections.singletonList("System"), Collections
+		        .<String> emptyList()));
 	}
 }
--- Grader/trunk/org.cpntools.grader/src/org/cpntools/grader/signer/Signer.java
+++ Grader/trunk/org.cpntools.grader/src/org/cpntools/grader/signer/Signer.java
@@ -2,18 +2,27 @@ package org.cpntools.grader.signer;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+import org.cpntools.accesscpn.engine.highlevel.instance.InstanceFactory;
+import org.cpntools.accesscpn.engine.highlevel.instance.adapter.ModelData;
+import org.cpntools.accesscpn.engine.highlevel.instance.adapter.ModelDataAdapterFactory;
 import org.cpntools.accesscpn.model.Arc;
 import org.cpntools.accesscpn.model.HLDeclaration;
 import org.cpntools.accesscpn.model.HasId;
+import org.cpntools.accesscpn.model.Instance;
+import org.cpntools.accesscpn.model.Node;
 import org.cpntools.accesscpn.model.Page;
+import org.cpntools.accesscpn.model.ParameterAssignment;
 import org.cpntools.accesscpn.model.PetriNet;
 import org.cpntools.accesscpn.model.impl.PetriNetImpl;
+import org.cpntools.accesscpn.model.monitors.Monitor;
 import org.cpntools.grader.model.StudentID;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
@@ -27,8 +36,18 @@ public class Signer {
 	 * @param studentId
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	static public PetriNet sign(final PetriNetImpl model, final String secret, final StudentID studentId) {
 		final PetriNet result = EcoreUtil.copy(model);
+		final ModelData modelData = (ModelData) ModelDataAdapterFactory.getInstance().adapt(result, ModelData.class);
+		for (final Monitor m : result.getMonitors()) {
+			final Collection<Object> nodes = new ArrayList<Object>(m.getNodes());
+			m.getNodes().clear();
+			for (final Object o : nodes) {
+				final org.cpntools.accesscpn.engine.highlevel.instance.Instance<Node> i = (org.cpntools.accesscpn.engine.highlevel.instance.Instance<Node>) o;
+				m.getNodes().add(remap(i, modelData));
+			}
+		}
 		final Random random = getRandom(secret, studentId);
 		final Set<String> usedIds = new HashSet<String>();
 		final Map<String, String> idMap = new HashMap<String, String>();
@@ -44,7 +63,26 @@ public class Signer {
 				replaceId(random, usedIds, idMap, a);
 			}
 		}
+		for (final Page p : result.getPage()) {
+			for (final Instance i : p.instance()) {
+				i.setSubPageID(idMap.get(i.getSubPageID()));
+				for (final ParameterAssignment pa : i.getParameterAssignment()) {
+					pa.setParameter(idMap.get(pa.getParameter()));
+					pa.setValue(idMap.get(pa.getValue()));
+				}
+			}
+		}
 		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T extends Node> org.cpntools.accesscpn.engine.highlevel.instance.Instance<T> remap(
+	        final org.cpntools.accesscpn.engine.highlevel.instance.Instance<T> i, final ModelData modelData) {
+		if (i == null) { return null; }
+		final org.cpntools.accesscpn.engine.highlevel.instance.Instance<Node> result = InstanceFactory.INSTANCE
+		        .createInstance(modelData.getNode(i.getNode().getId()), i.getInstanceNumber());
+		result.setTransitionPath(remap(i.getTransitionPath(), modelData));
+		return (org.cpntools.accesscpn.engine.highlevel.instance.Instance<T>) result;
 	}
 
 	private static Random getRandom(final String secret, final StudentID studentId) {
