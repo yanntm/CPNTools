affa0fb42ac942edb495ef4528cede4b9230b09d
mwesterg <mwesterg@83e89a13-d7ff-4fff-b021-40a13f624d75>
2012-05-06 20:32:12 +0000

    Working BTL tester

--- Grader/trunk/org.cpntools.grader/src/org/cpntools/grader/gui/BTLTester.java
+++ Grader/trunk/org.cpntools.grader/src/org/cpntools/grader/gui/BTLTester.java
@@ -4,11 +4,19 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedMap;
 import java.util.SortedSet;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 import javax.swing.BorderFactory;
@@ -27,29 +35,74 @@ import javax.swing.table.DefaultTableModel;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.cpntools.accesscpn.engine.highlevel.HighLevelSimulator;
+import org.cpntools.accesscpn.engine.highlevel.instance.Binding;
 import org.cpntools.accesscpn.engine.highlevel.instance.Instance;
 import org.cpntools.accesscpn.engine.highlevel.instance.adapter.ModelInstance;
 import org.cpntools.accesscpn.engine.highlevel.instance.adapter.ModelInstanceAdapterFactory;
-import org.cpntools.accesscpn.model.Page;
 import org.cpntools.accesscpn.model.PetriNet;
 import org.cpntools.accesscpn.model.Transition;
 import org.cpntools.accesscpn.model.importer.DOMParser;
 import org.cpntools.accesscpn.model.importer.NetCheckException;
+import org.cpntools.grader.model.NameHelper;
 import org.cpntools.grader.model.StudentID;
+import org.cpntools.grader.model.btl.BTLGrader;
 import org.cpntools.grader.model.btl.model.Guide;
+import org.cpntools.grader.model.btl.model.Unconsumed;
 import org.cpntools.grader.model.btl.parser.CupParser;
 import org.cpntools.grader.tester.EnablingControl;
 import org.cpntools.grader.tester.EnablingControlAdapterFactory;
 import org.xml.sax.SAXException;
 
 public class BTLTester extends JDialog {
+	public static class Snapshot {
+
+		private final String time;
+		private final Binding be;
+		private final Guide guide;
+
+		public Snapshot(final String time, final Binding be, final Guide guide) {
+			this.time = time;
+			this.be = be;
+			this.guide = guide;
+		}
+
+		public Guide getGuide() {
+			return guide;
+		}
+
+		public Binding getBinding() {
+			return be;
+		}
+
+		/**
+		 * @return
+		 */
+		public String getTime() {
+			return time;
+		}
+
+		@Override
+		public String toString() {
+			if (be == null) { return "<initial state>"; }
+			return be.toString();
+		}
+
+	}
+
 	private final PetriNet petriNet;
 	private final JTextArea marking;
 	private HighLevelSimulator simulator;
 	Guide current = null;
 	private final JTextArea currentFormula;
 	private final JTextArea parsedFormula;
-	private final JTextArea initFormula;
+	final JTextArea initFormula;
+	private final ModelInstance modelInstances;
+	private final List<Instance<Transition>> allTransitionInstances;
+	private final NameHelper nameHelper;
+	private final EnablingControl ec;
+	private final DefaultListModel disallowed;
+	final DefaultListModel enabled;
+	private final DefaultTableModel trace;
 
 	public BTLTester(final File selectedFile) throws FileNotFoundException, NetCheckException, SAXException,
 	        IOException, ParserConfigurationException {
@@ -60,7 +113,9 @@ public class BTLTester extends JDialog {
 		final JPanel buttons = new JPanel();
 		buttons.setLayout(new FlowLayout(FlowLayout.CENTER));
 		add(buttons, BorderLayout.SOUTH);
-		buttons.add(new JButton("Check"));
+		final JButton checkButton = new JButton("Check");
+		checkButton.setEnabled(false);
+		buttons.add(checkButton);
 		final JButton refreshButton = new JButton("Refresh");
 		refreshButton.addActionListener(new ActionListener() {
 			@Override
@@ -78,8 +133,11 @@ public class BTLTester extends JDialog {
 		});
 		buttons.add(initialButton);
 
+		final JSplitPane top = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		top.setResizeWeight(0.5);
+		add(top, BorderLayout.CENTER);
 		final JSplitPane main = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-		add(main, BorderLayout.CENTER);
+		top.setLeftComponent(main);
 		final JPanel formulae = new JPanel();
 		formulae.setLayout(new BoxLayout(formulae, BoxLayout.Y_AXIS));
 		final JPanel formulaPanel = new JPanel(new BorderLayout());
@@ -98,7 +156,17 @@ public class BTLTester extends JDialog {
 		final JScrollPane transitionMappingScroller = new JScrollPane(new JTable(new DefaultTableModel(new Object[] {
 		        "ID", "Transition" }, 0)));
 		transitionMappingScroller.setBorder(BorderFactory.createTitledBorder("Transition Mapping"));
-		add(transitionMappingScroller, BorderLayout.EAST);
+		final JSplitPane rightTransitions = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		rightTransitions.setTopComponent(transitionMappingScroller);
+		top.setRightComponent(rightTransitions);
+		rightTransitions.setResizeWeight(0.5);
+
+		trace = new DefaultTableModel(new Object[] { "Time", "Binding Element" }, 0);
+		final JTable traceTable = new JTable(trace);
+		traceTable.setEnabled(false);
+		final JScrollPane traceScroller = new JScrollPane(traceTable);
+		traceScroller.setBorder(BorderFactory.createTitledBorder("Execution Trace"));
+		rightTransitions.setBottomComponent(traceScroller);
 
 		initFormula = new JTextArea(3, 80);
 		final JScrollPane initScroller = new JScrollPane(initFormula);
@@ -117,6 +185,23 @@ public class BTLTester extends JDialog {
 		currentFormula.setLineWrap(true);
 		formulae.add(currentScroller);
 
+		transitionList.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(final MouseEvent evt) {
+				final JList list = (JList) evt.getSource();
+				if (evt.getClickCount() == 2) { // Double-click
+					final int index = list.locationToIndex(evt.getPoint());
+					final String transitionName = ((String) transitionListModel.get(index)).toLowerCase().replaceAll(
+					        "[^a-z0-9.]*", "");
+					final int position = initFormula.getCaretPosition();
+					final String oldText = initFormula.getText();
+					initFormula.setText(oldText.substring(0, position) + transitionName + oldText.substring(position));
+					initFormula.setCaretPosition(position + transitionName.length());
+					initFormula.requestFocus();
+				}
+			}
+		});
+
 		marking = new JTextArea(10, 30);
 		marking.setEditable(false);
 		marking.setLineWrap(true);
@@ -128,37 +213,111 @@ public class BTLTester extends JDialog {
 
 		final JPanel transitions = new JPanel();
 		transitions.setLayout(new BoxLayout(transitions, BoxLayout.Y_AXIS));
-		final JScrollPane enabledScroller = new JScrollPane(new JList());
+		enabled = new DefaultListModel();
+		final JList enabledList = new JList(enabled);
+		enabledList.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(final MouseEvent evt) {
+				final JList list = (JList) evt.getSource();
+				if (evt.getClickCount() == 2) { // Double-click
+					final int index = list.locationToIndex(evt.getPoint());
+					final Binding clicked = (Binding) enabled.get(index);
+					execute(clicked);
+				}
+			}
+		});
+		final JScrollPane enabledScroller = new JScrollPane(enabledList);
 		enabledScroller.setBorder(BorderFactory.createTitledBorder("Enabled Bindings"));
 		transitions.add(enabledScroller);
-		final JScrollPane disallowedScroller = new JScrollPane(new JList());
+		disallowed = new DefaultListModel();
+		final JList disallowedList = new JList(disallowed);
+		disallowedList.setEnabled(false);
+		final JScrollPane disallowedScroller = new JScrollPane(disallowedList);
 		disallowedScroller.setBorder(BorderFactory.createTitledBorder("Disallowed Transitions"));
 		transitions.add(disallowedScroller);
 		modelData.setRightComponent(transitions);
 
-		initializeEnablingControl();
+		modelInstances = (ModelInstance) ModelInstanceAdapterFactory.getInstance().adapt(petriNet, ModelInstance.class);
+		allTransitionInstances = modelInstances.getModelData().getAllTransitionInstances();
+		nameHelper = new NameHelper(petriNet);
+
+		ec = (EnablingControl) EnablingControlAdapterFactory.getInstance().adapt(petriNet, EnablingControl.class);
 		initializeTransitionList(transitionListModel);
+		clearTrace();
 		pack();
 		setVisible(true);
 		try {
 			simulator = org.cpntools.grader.tester.Tester.checkModel(petriNet, selectedFile.getParentFile(),
 			        selectedFile.getParentFile(), new StudentID("dummy"));
 			refreshMarking();
+			refreshEnabling();
 		} catch (final Exception e) {
 			marking.setText("Checking failed with " + e);
 		}
 	}
 
-	public void initializeTransitionList(final DefaultListModel transitionListModel) {
-		final SortedSet<String> transitionNames = new TreeSet<String>();
-		final ModelInstance modelInstances = (ModelInstance) ModelInstanceAdapterFactory.getInstance().adapt(petriNet,
-		        ModelInstance.class);
-		for (final Page p : petriNet.getPage()) {
-			for (final Transition t : p.transition()) {
-				for (final Instance<Transition> ti : modelInstances.getAllInstances(t)) {
-					transitionNames.add(ti.toString());
+	protected void execute(final Binding be) {
+		try {
+			simulator.execute(be);
+			if (current != null) {
+				current = current.progress(be.getTransitionInstance(), petriNet, simulator, nameHelper);
+			}
+			addToTrace(simulator.getTime(), be, current);
+			refreshMarking();
+			refreshEnabling();
+			refreshCurrent();
+		} catch (final IOException e) {
+			marking.setText("Execution failed with: " + e);
+		} catch (final Unconsumed e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void addToTrace(final String time, final Binding be, final Guide guide) {
+		trace.addRow(new Object[] { time, new Snapshot(time, be, guide) });
+	}
+
+	@SuppressWarnings("unchecked")
+	private void refreshEnabling() {
+		try {
+			final Set<Instance<Transition>> allowed = new HashSet<Instance<Transition>>();
+			List<Instance<? extends Transition>> enabled;
+			if (current != null) {
+				enabled = BTLGrader.getEnabledAndAllowed(petriNet, simulator, nameHelper, allTransitionInstances, ec,
+				        current, allowed);
+			} else {
+				enabled = BTLGrader.getEnabled(simulator, allTransitionInstances, ec);
+				allowed.addAll((Collection<? extends Instance<Transition>>) enabled);
+			}
+			final SortedMap<String, Instance<? extends Transition>> sortedNames = new TreeMap<String, Instance<? extends Transition>>();
+			for (final Instance<? extends Transition> ti : enabled) {
+				Instance<? extends Transition> old = sortedNames.put(ti.toString(), ti);
+				while (old != null) {
+					old = sortedNames.put(old.toString() + "#", old);
 				}
 			}
+			this.enabled.clear();
+			disallowed.clear();
+			for (final Instance<? extends Transition> ti : sortedNames.values()) {
+				if (allowed.contains(ti)) {
+					for (final Binding b : simulator.getBindings(ti)) {
+						this.enabled.addElement(b);
+					}
+				} else {
+					disallowed.addElement(ti);
+				}
+			}
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void initializeTransitionList(final DefaultListModel transitionListModel) {
+		final SortedSet<String> transitionNames = new TreeSet<String>();
+		for (final Instance<Transition> ti : allTransitionInstances) {
+			transitionNames.add(ti.toString());
 		}
 		for (final String name : transitionNames) {
 			transitionListModel.addElement(name);
@@ -172,10 +331,12 @@ public class BTLTester extends JDialog {
 	void refresh() {
 		reparseCurrent();
 		refreshMarking();
+		refreshEnabling();
 		refreshCurrent();
 	}
 
 	void initial() {
+		clearTrace();
 		reparse();
 		try {
 			simulator.initialState();
@@ -183,6 +344,13 @@ public class BTLTester extends JDialog {
 			marking.setText("Could not go to the initial state. " + e);
 		}
 		refresh();
+	}
+
+	private void clearTrace() {
+		while (trace.getRowCount() > 0) {
+			trace.removeRow(trace.getRowCount() - 1);
+		}
+		addToTrace("0", null, null);
 	}
 
 	private void reparseCurrent() {
